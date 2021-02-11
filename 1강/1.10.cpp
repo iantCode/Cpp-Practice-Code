156cc146e7a55abe20002aa4f81169e90d51b972
iantCode <ahj010216@naver.com>
2021-02-11 12:38:13 +0900

    1.10강 학습 완료

---
+++ 1강/1.10.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+int add(int a, int b)
+{
+    return a + b;
+}
+int subtract(int a, int b); //forward declaration
+int multiply(int, int);
+
+
+int main()
+{
+    std::cout << add(1, 2) <<std::endl;
+    std::cout << subtract(2, 1) <<std::endl;
+    std::cout << multiply(2, 3) <<std::endl;
+}
+
+int subtract(int a, int b)
+{
+    return a - b;
+}
+
+int multiply(int a, int b)
+{
+    return a * b;
+}
