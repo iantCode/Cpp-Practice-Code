4fcd81d6e42d44ae14fe623e07764a5d9a275aa0
iantCode <ahj010216@naver.com>
2021-02-11 12:33:20 +0900

    1.9강 학습 완료

---
+++ 1강/1.9.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+//int add(int x, int y) { return x + y; } //가능하나 읽기 조금 불편함.
+int add(int x, int y)
+{
+    return x + y;
+}
+
+
+int main()
+{
+    std::cout << "Hello, World! " <<
+        "Good Morning, World!" << std::endl;
+
+    return 0;
+}
